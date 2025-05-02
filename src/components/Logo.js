826fcb8f6c899d51611f6e0fdea14f3ec189bd7e
littlesweetitmysm <busispec0101@tutamail.com>
2025-05-02 15:30:45 -0700

    Fix keyframes import in Logo component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,4 +1,5 @@
-import { Box, Text, HStack, keyframes } from '@chakra-ui/react';
+import { Box, Text, HStack } from '@chakra-ui/react';
+import { keyframes } from '@emotion/react';
 
 const gradientAnimation = keyframes`
   0% { background-position: 0% 50%; }
